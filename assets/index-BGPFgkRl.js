8ee3b213d7a2884c5932a0ef041c4a9bdd9d247c
Janajith-D <144233877+Janajith-D@users.noreply.github.com>
2025-06-17 18:03:49 +0530

    Updates

--- assets/index-BGPFgkRl.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./jsx-runtime-DPlTSVOs.js";function i(){return e.jsx("div",{children:"Hello ImageViewer"})}export{i as I};
--- assets/index-C5ucqr1h.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./jsx-runtime-DPlTSVOs.js";function n(){return e.jsx("div",{children:"Hello IrexSequence"})}export{n as I};
